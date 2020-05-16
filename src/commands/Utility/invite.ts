ea59fe5821725e4e6cdb7d16e7276279e276d105
Naia <5724342-Naia-love@users.noreply.gitlab.com>
2020-05-16 22:51:07 +0200

    reger

---
+++ src/commands/Utility/invite.ts
@@ -0,0 +1,20 @@
+import { Command, Message } from '../../Client';
+
+const callback = async (message: Message, _args: string[]) => {
+	let client = message.client;
+	message.channel.send(`https://discordapp.com/oauth2/authorize?client_id=${message.client.user.id}&permissions=1580592383&scope=bot`);
+};
+
+export const command: Command = {
+	name: 'invite',
+	category: 'Utility',
+	aliases: ['inv'],
+	usage: '',
+	args: 0,
+	devOnly: false,
+	guildOnly: false,
+	nsfw: false,
+	memberPermission: [],
+	botPermission: [],
+	callback: callback
+};
