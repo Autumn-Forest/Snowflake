ae80444464c02f22a715bf6b29fe0dee7cd64a59
Naia <5724342-Naia-love@users.noreply.gitlab.com>
2020-05-16 22:51:39 +0200

    ughh

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,12 @@
 export const config: Config = require(`./${process.env.NODE_ENV}`).default;
 
 interface Config {
-    token: string;
-    mongoString: string;
-    defaultPrefix: string;
-    developers: string[];
-    channels: {
-        info: string;
-        errors: string;
-    };
+	token: string;
+	mongoString: string;
+	defaultPrefix: string;
+	developers: string[];
+	channels: {
+		info: string;
+		errors: string;
+	};
 }
