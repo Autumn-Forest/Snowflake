64bc25f12b5396908a93f823c42d29c4ce2606f1
Mattis6666 <sgtmattis@gmail.com>
2020-05-17 00:13:19 +0200

    Production

--- src/config/production.ts
+++ src/config/production.ts
@@ -1,10 +1,10 @@
 export default {
-	defaultPrefix: '',
-	token: '',
-	mongoString: '',
-	developers: [],
+	defaultPrefix: '*',
+	token: process.env.SNOWFLAKE_TOKEN,
+	mongoString: process.env.SNOWFLAKE_DATABASE,
+	developers: ['265560538937819137', '390977127153139733', '279910519467671554'],
 	channels: {
-		info: '',
-		errors: ''
+		info: '711228013265813524',
+		errors: '711228030982422529'
 	}
 };
