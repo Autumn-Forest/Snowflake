88bf9a9a781373689f841b3dcfdd85c48273d673
Naia-love <love.naia@protonmail.com>
2020-05-17 19:07:14 +0200

    quick owofy

---
+++ src/commands/fun/owofy.ts
@@ -0,0 +1,24 @@
+import { Command, Message } from '../../Client';
+
+const callback = async (msg: Message, args: string[]) => {
+	const owo = await msg.client.nekos.OwOify(args.join(' '));
+	if (!owo) return msg.client.helpers.wrongSyntax(msg, 'You did not use this command correctly. The correct usage is `{USAGE}`.');
+	msg.delete();
+	const ee = msg.client.newEmbed('BASIC').setTitle(`${msg.author.tag} said:`).setDescription(owo);
+	return msg.channel.send(ee);
+};
+
+export const command: Command = {
+	name: 'OwOfy',
+	category: 'fun',
+	aliases: ['owo'],
+	description: 'OwOfy youw text uwu',
+	usage: '<text>',
+	args: 1,
+	devOnly: false,
+	guildOnly: false,
+	nsfw: false,
+	memberPermission: [],
+	botPermission: [],
+	callback: callback
+};
