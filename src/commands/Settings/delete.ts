534c26b2627819768dbb09bcf57646b2a82c5b7e
Mahruan <sgtmattis@gmail.com>
2020-05-18 01:26:18 +0200

    Update delete.ts

--- src/commands/Settings/delete.ts
+++ src/commands/Settings/delete.ts
@@ -7,22 +7,11 @@ const callback = async (msg: Message, args: string[]) => {
 	const arg = args[0].toLowerCase() === 'on';
 	settings.settings.deleteCommandTriggers = arg;
 	settings.save();
-	if (arg)
-		return msg.channel.send(
-			`I'll now delete the commmand trigger :D\nDo \`${
-				settings.settings.prefix || msg.client.config.defaultPrefix
-			}cmddelete\` to desactivate this functionnality`
-		);
-	else
-		return msg.channel.send(
-			`I'll now stop to delete the command trigger :D\nDo \`${
-				settings.settings.prefix || msg.client.config.defaultPrefix
-			}cmddelete on\` to activate this functionnality`
-		);
+	return msg.channel.send(`I will ${arg ? 'now' : 'no longer'} delete commmand triggers :D`);
 };
 
 export const command: Command = {
-	name: 'Command delete',
+	name: 'deletecommandtriggers',
 	category: 'Settings',
 	aliases: ['cmddelete', 'triggerdelete'],
 	description: 'Delete the triggers of the commands.',
