fe5e7c5d812d40bfd5e153399cb53a1aa330da89
Mattis6666 <sgtmattis@gmail.com>
2020-05-17 15:41:45 +0200

    This is ugly

--- src/Client/Helpers/index.ts
+++ src/Client/Helpers/index.ts
@@ -1,42 +1,11 @@
-import { Message } from '..';
-import Getters from './getters';
-import nodeFetch, { RequestInfo, RequestInit } from 'node-fetch';
+import { Getters } from './getters';
+import { Client } from '..';
 
-export class ClientHelpers extends Getters {
-	async wrongSyntax(message: Message, text: string, del = false) {
-		const msg = await message.reply(text);
-		if (!msg.guild || !del) return;
-
-		msg.delete({ timeout: 1000 * 10 }).catch(() => null);
-		message.delete({ timeout: 1000 * 10 }).catch(() => null);
+export class BaseClass {
+	constructor(client: Client) {
+		this.client = client;
 	}
-
-	codeBlock(str: string, lang = 'js') {
-		return '```' + `${lang || ''}\n${str}` + '```';
-	}
-
-	trimString(str: string, len = 2000) {
-		return str.length > len ? str.slice(0, len - 3) + '...' : str;
-	}
-
-	async fetch(requestInfo: RequestInfo, requestOptions?: RequestInit) {
-		const result = await nodeFetch(requestInfo, requestOptions)
-			.then(response => {
-				return response.json().then(json => {
-					return response.ok ? json : Promise.reject(json);
-				});
-			})
-			.catch(this.client.handleError);
-		return result;
-	}
-
-	async uploadHaste(text: string) {
-		const result = await this.fetch('https://hasteb.in/documents', {
-			method: 'POST',
-			headers: { 'Content-Type': 'text/plain' },
-			body: text,
-			redirect: 'follow'
-		});
-		return result?.key ? `https://hasteb.in/${result.key}` : 'Failed to upload to hastebin!';
-	}
+	client: Client;
 }
+
+export class ClientHelpers extends Getters {}
