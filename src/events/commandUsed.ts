fb1d63fe6304e6d81b3caa5e4a755dd07139fb9e
Naia-love <love.naia@protonmail.com>
2020-05-18 01:28:46 +0200

    here!

--- src/events/commandUsed.ts
+++ src/events/commandUsed.ts
@@ -2,6 +2,5 @@ import { Client, Message, Command } from '../Client';
 
 export const listener = async (_client: Client, msg: Message, _command: Command) => {
 	const s = await msg.client.getSettings(msg);
-	if (s?.settings.deleteCommandTriggers) return msg.delete({ timeout: 1000 }).catch(() => null);
-	return;
+	if (s?.settings.deleteCommandTriggers) msg.delete({ timeout: 1000 }).catch(() => null);
 };
