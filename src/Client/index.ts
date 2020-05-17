d2203c0acc6fcec884d6295b7315266da5df00d6
Mattis6666 <sgtmattis@gmail.com>
2020-05-17 20:20:51 +0200

    New commit

--- src/Client/index.ts
+++ src/Client/index.ts
@@ -52,7 +52,7 @@ export class Client extends BaseClient {
 	colours: { [key in ClientColours]: string } = {
 		ERROR: 'FF403C',
 		INFO: '0D7DFF',
-		BASIC: 'A630FF'
+		BASIC: '75F1BD'
 	};
 	paths = {
 		listeners: join(__dirname, '../events'),
