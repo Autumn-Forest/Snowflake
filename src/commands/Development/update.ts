0559da1ebd1e89d04b9d4fa477db925ebe503436
Mattis6666 <sgtmattis@gmail.com>
2020-05-19 17:50:30 +0200

    Add update command: Pull, compile and reload or restart if args provided

---
+++ src/commands/Development/update.ts
@@ -0,0 +1,31 @@
+import { Command, Message } from '../../Client';
+import { exec } from 'child_process';
+
+const callback = async (msg: Message, args: string[]) => {
+	let command = 'git pull && tsc';
+	if (args.length && ['yes', 'reload', '1', 'y'].includes(args[0].toLowerCase())) command += ' && pm2 restart Snowflake';
+
+	exec(command, (err, stdout, stderr) => {
+		if (err) msg.channel.send(msg.client.helpers.codeBlock(msg.client.helpers.trimString(err.stack || err.message, 2048)));
+		if (stderr) msg.channel.send(msg.client.helpers.codeBlock(msg.client.helpers.trimString(stderr, 2048)));
+		if (stdout) msg.channel.send(msg.client.helpers.codeBlock(msg.client.helpers.trimString(stdout, 2048)));
+		if (!err && !stderr && !args.length) {
+			msg.client.getCommand('reload')?.callback(msg, args);
+		}
+	});
+};
+
+export const command: Command = {
+	name: 'update',
+	category: 'Dev',
+	aliases: [],
+	description: 'Apply the latest changes. git pull > tsc > reload',
+	usage: '[restart?]',
+	args: 0,
+	devOnly: true,
+	guildOnly: false,
+	nsfw: false,
+	memberPermission: [],
+	botPermission: [],
+	callback: callback
+};
