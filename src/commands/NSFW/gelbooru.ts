6e2b3633784fe90741a9bef76e8964f421abcddf
Naia-love <love.naia@protonmail.com>
2020-05-19 17:59:42 +0200

    gelb v1.1 *for the autocomplete tags, well fuck off, lazzy for now.* and did a const banned word *easier than have to change everytime*

--- src/commands/NSFW/gelbooru.ts
+++ src/commands/NSFW/gelbooru.ts
@@ -1,33 +1,44 @@
 import { Command, Message } from '../../Client';
 
 const callback = async (msg: Message, args: string[]) => {
+	args = args.map(arg => arg.toLowerCase());
+	const bannedTags = args.filter(arg => msg.client.constants.bannedTags.includes(arg));
+
+	if (bannedTags.length)
+		return msg.client.helpers.wrongSyntax(msg, `One or more of the provided tags are blacklisted as they break Discord ToS: ${bannedTags.join(', ')}`);
+
 	if (!args[0]) {
-		const file = await msg.client.helpers
-			.fetch(`https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=1&tags=sort:random&json=1${msg.client.config.gelbooruAPI}`)
-			.then(response => response.json());
+		const file = (
+			await msg.client.helpers.fetch(
+				`https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=100&tags=sort:random&json=1${msg.client.config.gelbooruAPI}`
+			)
+		)?.filter((item: { [key: string]: string }) => !msg.client.constants.bannedTags.some(word => item.tags.includes(word)));
+		const res = file[Math.floor(Math.random() * file.length)];
 		const e = msg.client
 			.newEmbed('BASIC')
 			.setTitle('gelbooru')
-			.setURL(`https://gelbooru.com/index.php?page=post&s=view&id=${file['0'].id}`)
-			.setImage(file[0].file_url);
+			.setURL(`https://gelbooru.com/index.php?page=post&s=view&id=${res.id}`)
+			.setImage(res.file_url);
 		return msg.channel.send(e);
 	} else {
 		const arg = args.join('+');
 		let errorc = 0;
-		const file = await msg.client.helpers
-			.fetch(`https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=1&tags=sort:random+${arg}&json=1${msg.client.config.gelbooruAPI}`)
-			.then(response => response.json())
-			.catch(() => {
-				errorc++;
-			});
+		const file = (
+			await msg.client.helpers
+				.fetch(`https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=sort:random+${arg}&json=1${msg.client.config.gelbooruAPI}`)
+				.catch(() => {
+					errorc++;
+				})
+		)?.filter((item: { [key: string]: string }) => !msg.client.constants.bannedTags.some(word => item.tags.includes(word)));
 		if (errorc < 1) {
+			const res = file[Math.floor(Math.random() * file.length)];
 			const e = msg.client
 				.newEmbed('BASIC')
 				.setTitle(arg.replace('+', ' '))
-				.setURL(`https://gelbooru.com/index.php?page=post&s=view&id=${file['0'].id}`)
-				.setImage(file[0].file_url);
+				.setURL(`https://gelbooru.com/index.php?page=post&s=view&id=${res.id}`)
+				.setImage(res.file_url);
 			return msg.channel.send(e);
-		} else return msg.channel.send("oop, one of your tags/combination of them surely don't exist!\nOr an error happened :/");
+		} else return msg.client.helpers.wrongSyntax(msg, "oop, one of your tags/combination of them surely don't exist!\nOr an error happened :/");
 	}
 };
 
