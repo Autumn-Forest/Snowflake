761ba91013c0d851e8bae4b9f0fa4af4520a2a2e
Mattis6666 <sgtmattis@gmail.com>
2020-05-19 17:40:14 +0200

    Add message edit log

---
+++ src/events/messageUpdate.ts
@@ -0,0 +1,26 @@
+import { Client, Message } from '../Client';
+import { TextChannel } from 'discord.js';
+
+export const listener = async (client: Client, oldMsg: Message, newMsg: Message) => {
+	if (oldMsg.partial) return;
+
+	if (oldMsg.author.bot || oldMsg.content === newMsg.content) return;
+
+	const settings = await client.cache.getGuild(newMsg);
+	if (!settings) return;
+	const logChannel = newMsg.guild?.channels.cache.get(settings?.channels.messageLogChannel);
+	if (!logChannel || !(logChannel instanceof TextChannel)) return;
+
+	const logEmbed = client
+		.newEmbed('INFO')
+		.setTitle('Message edited')
+		.setThumbnail(newMsg.author.displayAvatarURL({ dynamic: true }))
+		.addFields([
+			{ name: 'Channel', value: newMsg.channel, inline: true },
+			{ name: 'Message ID', value: `[${newMsg.id}](${newMsg.url})`, inline: true },
+			{ name: 'Before', value: newMsg.client.helpers.trimString(oldMsg.content || '-', 1024) },
+			{ name: 'After', value: newMsg.client.helpers.trimString(newMsg.content || '-', 1024) }
+		]);
+
+	return logChannel.send(logEmbed);
+};
