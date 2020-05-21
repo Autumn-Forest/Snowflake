97f9c85d2928d613b9d36d3490409cb135e99a58
Naia-love <love.naia@protonmail.com>
2020-05-21 16:24:06 +0200

    i push that if i have another pc to worl (won't be disponible untill a few days)

---
+++ src/commands/Utility/bio.ts
@@ -0,0 +1,45 @@
+import { Command, Message } from '../../Client';
+import { Bio } from 'discord.bio';
+
+const bio = new Bio();
+
+const callback = async (msg: Message, args: string[]) => {
+	const regex = msg.client.constants.regex.snowflake;
+	let user;
+	if (args[0]) user = msg.mentions.users.first()?.id || regex.exec(args[0])?.[0] || args[0];
+	else user = msg.author.id;
+	const info = await bio.details(user).catch(() => null);
+	if (!info)
+		return msg.client.helpers.wrongSyntax(
+			msg,
+			"No result? Try to tag an user or use a valid slug\n Or they may just don't have a profile on https://discord.bio"
+		);
+	user = await msg.client.users.fetch(info.discord.id).catch(() => null);
+	const embed = msg.client
+		.newEmbed('BASIC')
+		.setAuthor(
+			info.discord.username,
+			user?.displayAvatarURL({ dynamic: true }) || info.discord.displayAvatarURL,
+			`https://discord.bio/p/${info.settings.name}`
+		)
+		.setThumbnail(user?.displayAvatarURL({ dynamic: true }) || info.discord.displayAvatarURL)
+		.setTitle(`Bio of ${user?.tag || info.discord.tag}\n(\`${info.settings.name}\`)`)
+		.setURL(`https://discord.bio/p/${info.settings.name}`)
+		.setImage(info.settings.banner || 'https://your.is-inside.me/6PINEw1T.png');
+	msg.channel.send(embed);
+};
+
+export const command: Command = {
+	name: 'bio',
+	category: 'Utility',
+	aliases: ['discordbio'],
+	description: 'Show your or someone Discord.bio profile.',
+	usage: "[user|discord.bio's slug]",
+	args: 0,
+	devOnly: false,
+	guildOnly: false,
+	nsfw: false,
+	memberPermission: [],
+	botPermission: [],
+	callback: callback
+};
