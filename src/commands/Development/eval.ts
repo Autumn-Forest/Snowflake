207f8c7a0f60a9672ed95a21965c024ac891427d
Naia-love <love.naia@protonmail.com>
2020-06-01 13:28:29 +0200

    yeah i'm lazzy

--- src/commands/Development/eval.ts
+++ src/commands/Development/eval.ts
@@ -28,7 +28,7 @@ const callback = async (msg: Message, args: string[]) => {
 export const command: Command = {
 	name: 'eval',
 	category: 'Dev',
-	aliases: [],
+	aliases: ['ev'],
 	description: 'Eval script',
 	usage: '',
 	devOnly: true,
