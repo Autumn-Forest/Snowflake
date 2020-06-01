4c0efa27115a2ad3b81cefeee21e9f87d83eeef8
Naia-love <love.naia@protonmail.com>
2020-06-01 13:28:51 +0200

    this is better

--- src/commands/Utility/bio.ts
+++ src/commands/Utility/bio.ts
@@ -15,25 +15,36 @@ const callback = async (msg: Message, args: string[]) => {
 			"No result? Try to tag an user or use a valid slug\n Or they may just don't have a profile on https://discord.bio"
 		);
 	user = await msg.client.users.fetch(info.discord.id).catch(() => null);
+	let bd;
+	let created;
+	if (info.user.details.birthday) bd = new Date(info.user.details.birthday);
+	if (info.user.details.created_at) created = new Date(info.user.details.created_at);
+	const dateTimeFormat = new Intl.DateTimeFormat('en', {
+		year: 'numeric',
+		month: 'short',
+		day: '2-digit'
+	});
+	// prohack untill the wrapper is debugged
+	const slug = await msg.client.helpers.fetch(`https://api.discord.bio/v1/user/details/${info.discord.id}`).then(user => user.payload.user.details.slug);
 	const embed = msg.client
 		.newEmbed('BASIC')
 		.setAuthor(
 			info.discord.username,
 			user?.displayAvatarURL({ dynamic: true }) || info.discord.displayAvatarURL({ dynamic: true }),
-			`https://discord.bio/p/${info.user.details.name}`
+			`https://discord.bio/p/${slug}`
 		)
 		.setThumbnail(user?.displayAvatarURL({ dynamic: true }) || info.discord.displayAvatarURL({ dynamic: true }))
-		.setTitle(`Bio of ${user?.tag || info.discord.tag}\n(\`${info.user.details.name}\`)`)
-		.setURL(`https://discord.bio/p/${info.user.details.name}`)
+		.setTitle(`Bio of ${user?.tag || info.discord.tag}\n(\`${slug}\`)`)
+		.setURL(`https://discord.bio/p/${slug}`)
 		.setImage(info.user.details.banner || 'https://your.is-inside.me/6PINEw1T.png')
 		.setDescription(`**About:** ${info.user.details.description}\n**Upvote:** ${info.user.details.upvotes}`)
 		.addFields(
 			{ name: 'Location', value: info.user.details.location || 'not set', inline: true },
-			{ name: 'Birthday', value: info.user.details.birthday?.slice(0, 9) || 'not set', inline: true },
+			{ name: 'Birthday', value: dateTimeFormat.format(bd) || 'not set', inline: true },
 			{ name: 'gender', value: info.user.details.gender || 'not set', inline: true },
 			{ name: 'email', value: info.user.details.email || 'not set', inline: true },
 			{ name: 'occupation', value: info.user.details.occupation || 'not set', inline: true },
-			{ name: 'Created at', value: info.user.details.created_at?.slice(0, 9) || 'not set', inline: true }
+			{ name: 'Created at', value: dateTimeFormat.format(created) || 'not set', inline: true }
 		);
 	return msg.channel.send(embed);
 };
