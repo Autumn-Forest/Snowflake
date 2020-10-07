1456aad261d46eb829dada723bcc1deaf73c12e3
Venny <sgtmattis@gmail.com>
2020-10-07 22:37:06 +0200

    Update env namems

--- src/config/production.ts
+++ src/config/production.ts
@@ -1,7 +1,7 @@
 export default {
 	defaultPrefix: '*',
-	token: process.env.TOKEN,
-	mongoString: process.env.DATABASE,
+	token: process.env.SNOWFLAKE_TOKEN,
+	mongoString: process.env.SNOWFLAKE_DATABASE,
 	developers: ['343383572805058560', '390977127153139733', '279910519467671554'],
 	channels: {
 		info: '711228013265813524',
